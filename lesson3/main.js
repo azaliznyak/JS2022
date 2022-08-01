d7b7634e91b736af950250d575edf8076f324012
andrianazaliznak <andrianazaliznyak@gmail.com>
2022-08-01 23:52:47 +0300

    update

--- lesson3/main.js
+++ lesson3/main.js
@@ -23,12 +23,62 @@
 // }
 
 // - У змінній day дано якесь число від 1 до 31. Потрібно визначити, у яку половину(декаду) місяця потрапляє це число (у першу, другу чи третю).
+// let day=19;
+// if (day>0 && day<=10){
+//     console.log('first part')
+// }else if (day>10 && day<=20){
+//     console.log('second part')
+// }else if (day>20 && day<=31){
+//     console.log('third part')
+// }else {
+//     console.log('?????')
+// }
 //
 // - Скласти розклад на тиждень за домопоги switch. Користувач вводить порядковий номер дня тижня і на екрані відображається інфа що заплановано на цей день (можна замість плану на день, назву дня англійською).
+// let days=6;
+// switch (days){
+//     case 1:
+//         console.log('learning english');
+//         break;
+//     case 2:
+//         console.log('learning french');
+//         break;
+//     case 3:
+//         console.log('programing');
+//         break;
+//     case 4:
+//         console.log('lecture, university assignment')
+//         break;
+//     case 5:
+//         console.log('practice, studying')
+//         break;
+//     case 6:
+//         console.log('meet friends')
+//         break;
+//     case 7:
+//         console.log('practice, studying')
+//         break;
+//     default:
+//         console.log('??????')
+//
+// }
 //
 //     - Користувач вводить або має два числа.
 //         Потрібно знайти та вивести максимальне число з тих двох .
 //         Також потрібно врахувати коли введені рівні числа.
+
+// let a=55;
+// let b=55;
+// if (a>b && b<a){
+//     console.log(a)
+// }else if (a<b && b>a){
+//     console.log(b)
+// }else {
+//     console.log('they are equal')
+// }
 //
 //     - є змінна х, яка може прийняти будь-яке значення (стрінг, число, undefined, null  і тд включно). Напишіть код який,
 //         за допомоги  оператора || буде присвоювати змінній х значення "default"  якщо значення змінної х являється falsy (хибноподыбне, тобто кастується до false)
+
+// let x= ''|| 'default';
+// console.log(x)
