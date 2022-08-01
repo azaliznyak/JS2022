6f6926fb47d20fdd9ba9382f3fbeeb8880ddc8f1
andrianazaliznak <andrianazaliznyak@gmail.com>
2022-08-01 23:13:02 +0300

    Initial commit

---
+++ lesson1/main.js
@@ -0,0 +1,92 @@
+// - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//     Вивести кожну змінну за допомогою: console.log , alert, document.write
+// let hello ='helloooooo';
+// console.log(hello)
+// alert(hello)
+// document.write(`${hello}`)
+
+
+// let owu='owu';
+// console.log(owu)
+// alert(owu)
+// document.write(`${owu}`)
+//
+// let com= 'com';
+// console.log(com)
+// alert(com)
+// document.write(`${com}`)
+//
+// let ua ='ua'
+// console.log(ua)
+// alert(ua)
+// document.write(`${ua}`)
+//
+// let one = 1
+// console.log(one)
+// alert(one)
+// document.write(`${one}`)
+//
+// let ten = 10
+// console.log(ten)
+// alert(ten)
+// document.write(`${ten}`)
+//
+// let minus = -999
+// console.log(minus)
+// alert(minus)
+// document.write(`${minus}`)
+//
+// let one23 = 123
+// console.log(one23)
+// alert(one23)
+// document.write(one23)
+//
+// let three= 3.14
+// console.log(three)
+// alert(three)
+// document.write(`${three}`)
+//
+// let two= 2.7
+// console.log(two)
+// alert(two)
+// document.write(`${two}`)
+//
+// let sixteen = 16
+// console.log(sixteen)
+// alert(sixteen)
+// document.write(`${sixteen}`)
+//
+// let truth= true
+// console.log(truth)
+// alert(truth)
+// document.write(`${truth}`)
+//
+// let lie= false
+// console.log(lie)
+// alert(lie)
+// document.write(`${lie}`)
+
+//
+// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації);
+// let firstName='Andriana';
+// let middleName='Olegivna';
+// let lastName='Zaliznyak';
+// let person=`${firstName} ${middleName} ${lastName}`;
+// console.log(person)
+
+//
+// - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//     let a = 100; let b = '100'; let c = true;
+//     console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c)
+
+//
+// - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками. та вивести в консольxa
+// let userName=prompt('Enter your name');
+// console.log(userName);
+// let middleName=prompt('Enter your Middlename');
+// console.log(middleName);
+// let age=+prompt('Enter your age');
+// console.log(age)
+// document.write(`${userName} ${middleName} ${age}`)
---
+++ lesson2/main.js
@@ -0,0 +1,102 @@
+// // // - Створити масив, наповнити його 10 елементами будь-якого типу, вивести кожен елемент в консоль
+// // let arr=['hello', 55,true,'flower','sky','glass',789,false,'oktem','js'];
+// // for (let arrElement of arr) {
+// //     console.log(arrElement)
+// // }
+// // console.log(arr[0])
+// // console.log(arr[1])
+// // // - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre.
+// // let bookFirst={
+// //     title:'Brief answer to the big question',
+// //     pageCount:400,
+// //     genre:'scientific'
+// // }
+// //
+// // let bookSecond={
+// //     title:'Upstream',
+// //     pageCount:200,
+// //     genre:'studies'
+// // }
+// //
+// // let bookThird={
+// //     title:'The richest man in Babylon',
+// //     pageCount:170,
+// //     genre:'Non-fiction'
+// // }
+// // - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre, authors. Поле "автори" - масив. Кожен автор має поля name,age
+// // let bookFirst={
+// //     title:'Brief answer to the big question',
+// //     pageCount:400,
+// //     genre:'scientific',
+// //     authors:[
+// //         {name:'Stephen Hawking', age:76},
+// //         {name:'Charles Dickens', age: 58}
+// //     ]
+// // }
+// //
+// // let bookSecond={
+// //     title:'Upstream',
+// //     pageCount:200,
+// //     genre:'studies',
+// //     authors:[
+// //         {name:'Dan Heath', age:49},
+// //         {name:'Seth Godin', age: 62}
+// //     ]
+// // }
+// //
+// // let bookThird={
+// //     title:'The richest man in Babylon',
+// //     pageCount:170,
+// //     genre:'Non-fiction',
+// //     authors:[
+// //         {name:'George Samuel Clason', age:83},
+// //         {name:'George Orwell', age: 47}
+// //     ]
+// // }
+//
+// // - Створити масив з 10 об'єктами які описують сутніть "користувач". Поля: name, username,password. Вивести в консоль пароль кожного користувача
+// //  let users=[
+// //      {name:'Olya',
+// //      username:'Koksharova',
+// //      password:'fdjf787fh'
+// //      },
+// //      {name:'Oksana',
+// //          username:'Zaliznyak',
+// //          password:'hfj8'
+// //      },
+// //      {name:'Sasha',
+// //          username:'Nikitin',
+// //          password:'jdbcj09'
+// //      },
+// //      {name:'Tanya',
+// //          username:'Shevchenko',
+// //          password:'dhdjnc66'
+// //      },
+// //      {name:'Ann',
+// //          username:'Boyko',
+// //          password:'dgdhdlln21343n'
+// //      },
+// //      {name:'Andry',
+// //          username:'Dmitrik',
+// //          password:'njbdfhkkkkkkk'
+// //      },
+// //      {name:'Roksolana',
+// //          username:'Telyuk',
+// //          password:'nnnnnnnnn'
+// //      },
+// //      {name:'Maks',
+// //          username:'Gritsak',
+// //          password:'jhgfda12344'
+// //      },
+// //      {name:'Denys',
+// //          username:'Lapkevych',
+// //          password:'ghjk1111111'
+// //      },
+// //
+// //  ]
+// //
+// // console.log(users[0].password)
+// //
+// // for (let user of users) {
+// //     console.log(user.password)
+// }
